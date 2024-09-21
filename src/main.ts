ee28361ca04511ad02a3f96110711824ff88e5e8
Saud <saud.s@engees.in>
2024-09-21 17:54:47 +0530

    completed

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://react-frontend-gray-five.vercel.app'], 
+    origin: ['http://localhost:3000', 'https://react-frontend-gray-five.vercel.app', 'https://react-todo-production-3662.up.railway.app'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   })
